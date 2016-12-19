91513681551a4828c4bfb381f50006e34425e890
yahier <xuziwang@stbl.cc>
2016-12-19 10:21:24 +0800

    Initial commit

---
+++ java/src/main/java/com/example/MyClass.java
@@ -0,0 +1,13 @@
+package com.example;
+
+
+public class MyClass {
+    public static void  main(String[] args){
+        System.out.print("main");
+        a();
+    }
+
+    static void a(){
+        System.out.print("搞个毛");
+    }
+}

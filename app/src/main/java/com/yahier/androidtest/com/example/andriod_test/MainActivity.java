84e07923254df998a7bee845cc66532679471e7c
yahier <xuziwang@stbl.cc>
2016-12-20 17:05:18 +0800

    乱七八糟的测试

--- app/src/main/java/com/yahier/androidtest/com/example/andriod_test/MainActivity.java
+++ app/src/main/java/com/yahier/androidtest/com/example/andriod_test/MainActivity.java
@@ -1,56 +1,106 @@
 package com.yahier.androidtest.com.example.andriod_test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.yahier.androidtest.R;
+import com.yahier.androidtest.ver.tab.LoveAsk;
 
 
 import android.app.Activity;
-import android.database.Cursor;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	TextView text;
-	ShoucangDB db;
+    TextView text;
+    ShoucangDB db;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		text = (TextView) findViewById(R.id.text);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
 
-		db = new ShoucangDB(this);
-		
-		HandleAsync asyn = new HandleAsync();
-		asyn.excute(new HandleAsync.Listener() {
-
-			@Override
-			public void parse(String result) {
-				// TODO Auto-generated method stub
-				// System.out.println("result is "+result);
-				text.setText(result);
-				Cursor cursor = db.query("2.0120101E7");
-				cursor.moveToFirst();
-				while (!cursor.isAfterLast()) {
-					System.out.println("ũ��:"+cursor.getString(cursor.getColumnIndex(ShoucangDB.nongli)));
-					System.out.println("�� :"+cursor.getString(cursor.getColumnIndex(ShoucangDB.yi)));
-					
-					cursor.moveToNext();
-				}
-
-			}
-
-			@Override
-			public String getResult() {
-				// TODO Auto-generated method stub
-				return "";
-			}
-		});
-
-	}
+        db = new ShoucangDB(this);
 
 
+//        getWindow().getDecorView().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.e("run","finish");
+//                finish();
+//
+//
+//            }
+//        }, 5000);
+
+        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LoveAsk.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        getWindow().getDecorView().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                finish();
+//
+//
+//            }
+//        }, 3000);
+//    }
+
+    //mContext周期到底有多长。
+    void test1() {
+        final int a = 1;
+        final Context mContxt = MainActivity.this;
+        HandleAsync asyn = new HandleAsync();
+        asyn.excute(new HandleAsync.Listener() {
+
+            @Override
+            public void parse(String result) {
+                Log.e("act", "parse");
+                Context appContext = mContxt.getApplicationContext();
+                appContext.toString();
+                Log.e("act", "a:" + a);
+
+
+                final Context mContxt2 = MainActivity.this;
+                Context appContext2 = mContxt2.getApplicationContext();
+            }
+
+            @Override
+            public String getResult() {
+                Log.e("act", "getResult");
+                try {
+                    Thread.sleep(50000);
+                } catch (Exception e) {
+                    //pass
+                }
+                return "";
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("act", "onStop");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("act", "onDestroy");
+    }
 }

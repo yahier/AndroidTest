2089f9c342a63a2914259e8870694e5e860ff8d3
yahier <xuziwang@stbl.cc>
2017-01-08 17:40:11 +0800

    增加Canvas测试 save restore

--- app/src/main/java/com/yahier/androidtest/multipleThreads/OperateUiThreadAct.java
+++ app/src/main/java/com/yahier/androidtest/multipleThreads/OperateUiThreadAct.java
@@ -1,11 +1,13 @@
 package com.yahier.androidtest.multipleThreads;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.yahier.androidtest.R;
 
@@ -109,5 +111,13 @@ public class OperateUiThreadAct extends Activity {
                 });
             }
         }).start();
+        go();
+    }
+
+
+    void go(){
+        Toast.makeText(this,"执行go方法",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent("yahier.look");
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/yahier/androidtest/viewtest/CanvasTest.java
@@ -0,0 +1,63 @@
+package com.yahier.androidtest.viewtest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.yahier.androidtest.R;
+
+/**
+ * Created by yahier on 17/1/8.
+ */
+
+public class CanvasTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.canvas_test);
+
+        ViewCanvas view = new ViewCanvas(this);
+        setContentView(view);
+    }
+
+
+    public class ViewCanvas extends View {
+
+        public ViewCanvas(Context context) {
+            super(context);
+        }
+
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            testCanvasSaveAndRestore(canvas);
+        }
+    }
+
+
+    void testCanvasSaveAndRestore(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(10);
+        paint.setColor(getResources().getColor(R.color.white));
+        canvas.drawLine(100, 100, 800, 100, paint);
+
+        canvas.save();
+        canvas.rotate(5);
+        canvas.translate(200, 0);
+        canvas.scale(1.5f, 1.5f);
+
+        paint.setColor(getResources().getColor(R.color.red));
+        canvas.drawLine(100, 500, 800, 500, paint);
+
+
+        canvas.restore();
+
+        paint.setColor(getResources().getColor(R.color.blue));
+        canvas.drawLine(100, 900, 800, 900, paint);
+    }
+}

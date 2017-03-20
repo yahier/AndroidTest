3315871ee5d666d2e858b31f139d13fdb8117cc1
yahier <xuziwang@stbl.cc>
2017-03-20 13:56:48 +0800

    测试用bitmapFactory加载超大图。显示不能加载，最大只能8192*8192

--- app/src/main/java/com/yahier/androidtest/bitmap/ViewToBitmapAct.java
+++ app/src/main/java/com/yahier/androidtest/bitmap/ViewToBitmapAct.java
@@ -2,8 +2,10 @@ package com.yahier.androidtest.bitmap;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,13 +18,15 @@ import com.yahier.androidtest.R;
 
 public class ViewToBitmapAct extends Activity implements View.OnClickListener {
     View rootView;
+    ImageView img;
+    final String TAG = "ViewToBitmapAct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_2_bitmap_act);
         rootView = findViewById(R.id.root);
-
+        img = (ImageView) findViewById(R.id.img);
         findViewById(R.id.btn1).setOnClickListener(this);
         findViewById(R.id.btn2).setOnClickListener(this);
     }
@@ -31,10 +35,12 @@ public class ViewToBitmapAct extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn1:
-                ImageView img = (ImageView) findViewById(R.id.img);
                 img.setImageBitmap(createViewBitmap(rootView));
                 break;
             case R.id.btn2:
+                Bitmap bitmap = loadBigMap();
+                Log.e(TAG, "width:" + bitmap.getWidth());
+                img.setImageBitmap(bitmap);
                 break;
         }
 
@@ -46,5 +52,14 @@ public class ViewToBitmapAct extends Activity implements View.OnClickListener {
         Canvas canvas = new Canvas(bitmap);
         v.draw(canvas);
         return bitmap;
+    }
+
+    /**
+     * 图片太大，不能加载。只能加载到8192*8192
+     *
+     * @return
+     */
+    private Bitmap loadBigMap() {
+        return BitmapFactory.decodeResource(getResources(), R.drawable.qm);
     }
 }

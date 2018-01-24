3eb2570a531ea061ef60f40438a677d41b203542
yahier <xuziwang@stbl.cc>
2018-01-24 16:02:51 +0800

    测试聚合方法 reduce 和收集数据 collect 和原始数据流

--- java/src/main/java/java8/StreamApiTest.java
+++ java/src/main/java/java8/StreamApiTest.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -30,6 +34,10 @@ public class StreamApiTest {
         test4();
         test5();
         test6();
+        test7();
+        test8();
+        test9();
+        test10();
     }
 
     static void test1() {
@@ -45,11 +53,11 @@ public class StreamApiTest {
 
     //测试数组 转换成Stream
     static void test2() {
-        String[] arrays = {"徐子旺", "海飞", "陈桂容", "无双"};
+        String[] arrays = {"徐子旺", "海飞", "石矶娘娘", "无双"};
         long count = Stream.of(arrays).filter(str -> str.length() > 2).count();
         System.out.println("test2:" + count);
 
-        long count2 = Stream.of("徐子旺", "海飞", "陈桂容", "无双").count();
+        long count2 = Stream.of("徐子旺", "海飞", "石矶娘娘", "无双").count();
         System.out.println("test2:" + count2);
 
         long count3 = Arrays.stream(arrays, 0, 2).count();
@@ -57,7 +65,9 @@ public class StreamApiTest {
 
         //peek返回一个与原Stream相同元素的流。
         //toArray也很重要的函数咯
-        Object[] powers = Stream.iterate(1.0, p -> p * 2).peek(e -> System.out.println("e:" + e)).limit(5).toArray();
+        Object[] array1 = Stream.iterate(1.0, p -> p * 2).peek(e -> System.out.println("e:" + e)).limit(5).toArray();
+        //toArray方法也是可以有参数的
+        Integer[] array2 = Stream.iterate(1, p -> p * 2).limit(5).toArray(Integer[]::new);
     }
 
     //一些特别的Stream的生成 empty generate iterate splitAsStream
@@ -97,6 +107,64 @@ public class StreamApiTest {
     static void test6() {
         Stream<String> stream1 = list.stream().distinct().sorted(Comparator.comparingInt(String::length));
         println("test6", stream1);
+    }
+
+    //测试聚合方法 和 Optional
+    static void test7() {
+        //除了findFirst外，还有一个findAny.当前测试是同样的结果
+        Optional<String> optional = list.stream().filter(str -> str.contains("钟白大哥来了")).findFirst();
+        //如有存在数据
+        optional.ifPresent(s -> System.out.println("test7," + s));
+        //如果不存在数据的情况
+        String orElse = optional.orElse("no value 啊");//重新设值
+        //String exceptionStr = optional.orElseThrow(NoSuchElementException::new);//异常数据
+
+        //noneMatch 还有anyMatch
+        boolean result = list.stream().noneMatch(str -> str.contains("bingo"));
+        System.out.println("test7," + result);
+    }
+
+    //聚合操作(超厉害) (聚合 元素与顺序无关)  可以做到求和 求积 求列表最大值，列表最小值 追加内容...
+    static void test8() {
+        //测试求和
+        Stream<Integer> stream = Stream.iterate(1, value -> value + 1).limit(5);
+        Optional<Integer> result = stream.reduce((a, b) -> a + b);
+        result.ifPresent(v -> System.out.println("test8,result:" + v));
+        //测试求最大
+        int[] array = {1, 5, 77, 3, 68, 9};
+        //第一个操作是操作起始值
+        int result3 = Arrays.stream(array).reduce(0, (a, b) -> a >= b ? a : b);
+        System.out.println("test8,result:" + result3);
+        //测试追加内容
+        Stream<String> stream2 = list.stream();
+        Optional<String> result2 = stream2.reduce((a, b) -> a + b);
+        result2.ifPresent(v -> System.out.println("test8,result:" + v));
+    }
+
+    //收集结果 将stream转换称List Set String 或者Map(当前忽略)
+    static void test9() {
+        Stream<String> stream = list.stream();
+        List<String> list1 = stream.collect(Collectors.toList());
+        Set<String> set = stream.peek(v -> {
+        }).collect(Collectors.toSet());
+        System.out.println("test9-1," + list1.size() + ":" + set.size());//set还是会过滤的
+
+        //joining方法的参数是分隔符，可选
+        String result = stream.peek(v -> {
+        }).collect(Collectors.joining(","));
+        System.out.println("test9-2," + result);
+    }
+
+    //原始类型流IntStream LongStream DoubleStream
+    static void test10() {
+        IntStream intStream = IntStream.of(1, 3, 45, 6, 9);
+        int[] arrays = intStream.toArray();
+        //将int[] 转换成流，后面的参数可选
+        IntStream intStream1 = Arrays.stream(arrays, 2, 4);
+
+        IntStream intStream2 = IntStream.range(1, 100);
+
+        Stream<Integer> stream = intStream1.boxed();//将原始流转换成对象流
     }
 
 

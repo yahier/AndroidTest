815e4ecc363cb563ad129eed2a0ea22e9ae2e70e
xuziwang <xuziwang@xhg.com>
2018-12-19 14:50:41 +0800

    修改模板模式的测试(将模板方法都启动新的线程)

--- java/src/main/java/pattern/model/UpdateManager.java
+++ java/src/main/java/pattern/model/UpdateManager.java
@@ -2,41 +2,73 @@ package pattern.model;
 
 /**
  * Created by yahier on 12/19/18.
+ * Q1:怎么保证三个方法按固定顺序执行。
  */
 
 public class UpdateManager extends AbsMain {
     private final String TAG = "UpdateManager ";
+    private Object remux2 = new Object();
+    private Object remux3 = new Object();
+
 
     @Override
     protected void method1() {
-        System.err.println(TAG + "method1 开始");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.err.println(TAG + "method1 结束");
+        new Thread(() -> {
+            System.err.println(TAG + "method1 开始");
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.err.println(TAG + "method1 结束");
+            synchronized (remux2) {
+                remux2.notify();
+            }
+        }).start();
     }
 
     @Override
     protected void method2() {
-        System.err.println(TAG + "method2 开始");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        synchronized (remux2) {
+            try {
+                remux2.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.err.println(TAG + "method2 结束");
+
+        new Thread(() -> {
+            System.err.println(TAG + "method2 开始");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.err.println(TAG + "method2 结束");
+            synchronized (remux3) {
+                remux3.notify();
+            }
+        }).start();
     }
 
     @Override
     protected void method3() {
-        System.err.println(TAG + "method3 开始");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        synchronized (remux3) {
+            try {
+                remux3.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.err.println(TAG + "method3 结束");
+
+        new Thread(() -> {
+            System.err.println(TAG + "method3 开始");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.err.println(TAG + "method3 结束");
+        }).start();
     }
 }

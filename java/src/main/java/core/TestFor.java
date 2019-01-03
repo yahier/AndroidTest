cc4eb6970d4e62c359522b17eaa29df11d3f954f
xuziwang <xuziwang@xhg.com>
2019-01-03 17:44:37 +0800

    整合两个for循环的测试类

--- java/src/main/java/core/TestFor.java
+++ java/src/main/java/core/TestFor.java
@@ -1,5 +1,9 @@
 package core;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by yahier on 12/19/18.
  * for语句的测试
@@ -52,5 +56,24 @@ public class TestFor {
             System.out.println("test3:" + i2);
         }
 
+    }
+
+
+    private static void test4() {
+        Set<String> set = new HashSet<>();
+        set.add("晚风吹吹风");
+        set.add("我在吃西瓜");
+        set.add("门口池塘荷花摇啊摇");
+        set.add("鸟儿渣渣叫");
+        for (Iterator<String> iterator = set.iterator(); iterator.hasNext(); ) {
+            System.err.println(iterator.next());
+        }
+    }
+
+    //无限循环
+    private static void test5() {
+        for (; ; ) {
+            System.err.println(1);
+        }
     }
 }

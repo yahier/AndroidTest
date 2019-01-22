e77e1405b37fba0a72652cfd11625ac2bf50e68b
xuziwang <xuziwang@xhg.com>
2019-01-22 11:21:24 +0800

    小改命名

--- app/src/main/java/com/yahier/androidtest/metarial/MainAct.java
+++ app/src/main/java/com/yahier/androidtest/metarial/MainAct.java
@@ -7,14 +7,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.transition.Explode;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -28,7 +26,6 @@ import com.yahier.androidtest.bitmap.ChooseImgTestAct;
 import com.yahier.androidtest.bitmap.LargeImageViewActivity;
 import com.yahier.androidtest.bitmap.ViewToBitmapAct;
 import com.yahier.androidtest.classload.LoaderAct;
-import com.yahier.androidtest.common.MyApp;
 import com.yahier.androidtest.common.ReflectTest;
 import com.yahier.androidtest.content.provider.TestCPActivity;
 import com.yahier.androidtest.data.LogTest;
@@ -85,10 +82,9 @@ import java.util.TreeMap;
  */
 
 public class MainAct extends BaseActivity {
-
-    RecyclerView mRecyclerView;
-    TreeMap<String, MainItem> linkedHashMap;
-    MainAct mAct;
+    private RecyclerView mRecyclerView;
+    private TreeMap<String, MainItem> map;
+    private MainAct mAct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,7 +93,6 @@ public class MainAct extends BaseActivity {
         MaterialMainActBinding bind = DataBindingUtil.setContentView(MainAct.this, R.layout.material_main_act);
         mAct = this;
         mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
-
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         //加上间隔线。这个操作还有更多强大的功能
@@ -105,7 +100,6 @@ public class MainAct extends BaseActivity {
             @Override
             public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
                 super.onDraw(c, parent, state);
-
             }
 
 
@@ -135,9 +129,8 @@ public class MainAct extends BaseActivity {
             public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
                 super.getItemOffsets(outRect, view, parent, state);
             }
-
-
         });
+
         //引用失败
         Button btn = bind.tv;
         btn.setText("yahier的现实");
@@ -148,109 +141,94 @@ public class MainAct extends BaseActivity {
         setData();
         show();
         otherTest();
-        //testLink();
 
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
         ActivityCompat.requestPermissions(this, permissions, 99);
     }
 
-    void otherTest() {
+    /**
+     * 其他java方法的测试
+     */
+    private void otherTest() {
         ArrayMapSparseArrayUtil.main(null);
-        testApp();
 
-    }
-
-    //空指针 妥妥的
-    void testApp() {
-        String str = MyApp.getContext().getString(R.string.hello_world);
-        Log.e("testApp", str);
-    }
-
-    //跳转yaya:
-    private void testLink() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("yaya://www.baidu.com"));
-        startActivity(intent);
-        //测试命令行:11:51
     }
 
     /**
      * 请注意，显示效果是key大的，显示在上面
      */
     private void setData() {
-        linkedHashMap = new TreeMap<>((o1, o2) -> Integer.parseInt(o2) - Integer.parseInt(o1));
-        linkedHashMap.put("56", new MainItem("UI线程卡顿监听", "Looper打印 消息列表 Choreographer", MonitorUiBlockActivity.class));
-        linkedHashMap.put("55", new MainItem("apache common包测试", "BeanUtils CollectionsUtils", ApacheCommonTestAct.class));
-        linkedHashMap.put("54", new MainItem("线程池", "吼吼吼", ThreadPoolAct.class));
-
-        linkedHashMap.put("53", new MainItem("视频播放", "MediaPlayer配合surfaceView", MediaPlayerTest.class));
-        linkedHashMap.put("52", new MainItem("下载安装测试", "下载完成之后，自动安装此app", DownTest.class));
-        linkedHashMap.put("51", new MainItem("CollapsingToolbarLayout", "", CollapsingToolbarLayoutTest.class));
-        linkedHashMap.put("50", new MainItem("约束性布局", "ConstraintLayout", ConstraintLayoutTest.class));
-        linkedHashMap.put("49", new MainItem("状态栏适配变色", "", StatusBarFitColorAct.class));
-        linkedHashMap.put("48", new MainItem("link测试", "link", AppLinkTestAct.class));
-        linkedHashMap.put("47", new MainItem("binding测试", "6:绑定容器 数组", Test6Act.class));
-        linkedHashMap.put("46", new MainItem("binding测试", "5:View随model变化 而变化 ", Test5Act.class));
-        linkedHashMap.put("45", new MainItem("binding测试", "4:绑定多个相同的model ", Test4Act.class));
-        linkedHashMap.put("44", new MainItem("binding测试", "3:静态方法调用.运算符操作数据 ", Test3Act.class));
-        linkedHashMap.put("43", new MainItem("binding测试", "2:事件绑定 ", Test2Act.class));
-        linkedHashMap.put("42", new MainItem("binding测试", "1:测试数据绑定 ", Test1Act.class));
-
-        linkedHashMap.put("41", new MainItem("临时测试", "临时测试 ", MainActivity.class));
-        linkedHashMap.put("40", new MainItem("java upper", "lambda测试 ", JavaUpperAct.class));
-        linkedHashMap.put("39", new MainItem("WebView总结", "android与js的相互调用测试 ", WebViewAct.class));
-        linkedHashMap.put("1", new MainItem("矢量动画", "VectorSVG可缩放矢量动画 objectAnimator", VectorSVGAct.class));
-        linkedHashMap.put("2", new MainItem("事件分发测试", MotionEventAct.class));
-        linkedHashMap.put("3", new MainItem("drawable style效果", "详细解说一下", DrawableStyleAct.class));
-        linkedHashMap.put("4", new MainItem("drawable效果  包括BitmapShader", "详细解说一下", DrawableAct.class));
-        linkedHashMap.put("5", new MainItem("选图测试", "详细解说一下", ChooseImgTestAct.class));
-        linkedHashMap.put("6", new MainItem("左侧滑动", "详细解说一下", SwipeBackMainActivity.class));
-        linkedHashMap.put("7", new MainItem("自定义layout", "详细解说一下", MyLayoutAct.class));
-        linkedHashMap.put("8", new MainItem("自定义View", "详细解说一下", ViewLocationAct.class));
-        linkedHashMap.put("38", new MainItem("显示圆形图", "", CustomRoungImgAct.class));
-        linkedHashMap.put("9", new MainItem("SurfaceView测试", "详细解说一下", SurfaceViewTestAct.class));
-        linkedHashMap.put("10", new MainItem("测试广播", "详细解说一下", BroadCastActTest.class));
-        linkedHashMap.put("11", new MainItem("测试ConfigChanges", "详细解说一下", ConfigChangesActTest.class));
-        linkedHashMap.put("12", new MainItem("测试Handler处理", "详细解说一下", HandlerTest.class));
-        linkedHashMap.put("13", new MainItem("测试通知 类似微博效果", "详细解说一下", TestNotificationAct.class));
-        linkedHashMap.put("14", new MainItem("调通dex的方法", "详细解说一下", LoaderAct.class));
-        linkedHashMap.put("15", new MainItem("加载超大图", "详细解说一下", LargeImageViewActivity.class));
-        linkedHashMap.put("16", new MainItem("自定义ViewPager显示卷图", "详细解说一下", ParallaxVpTestActivity.class));
-        linkedHashMap.put("17", new MainItem("glide加载图片 测试", "详细解说一下", GlideTestAct.class));
-        linkedHashMap.put("18", new MainItem("material小特性", "translationZ  elevation", Material1Act.class));
-        linkedHashMap.put("19", new MainItem("recycle", "详细解说一下", RecycleAct.class));
-        linkedHashMap.put("20", new MainItem("cardview", "详细解说一下", CardViewAct.class));
-        linkedHashMap.put("21", new MainItem("service", "详细解说一下", ServiceActivity.class));
-        linkedHashMap.put("22", new MainItem("ContentProvider", "详细解说一下", TestCPActivity.class));
-        linkedHashMap.put("23", new MainItem("Synchonized", "测试synchonized", SynchonizedTest.class));
-        linkedHashMap.put("24", new MainItem("反射", "详细解说一下", ReflectTest.class));
-        linkedHashMap.put("25", new MainItem("messenger", "msgFromClient.replyTo = mMessenger", ActivityMessenger.class));
-        linkedHashMap.put("26", new MainItem("AccessbilityService", "详细解说一下", AccessServiceAct.class));
-        linkedHashMap.put("27", new MainItem("子线程操作UI", "详细解说一下", OperateUiThreadAct.class));
-        linkedHashMap.put("28", new MainItem("CanvasTest", "详细解说一下", CanvasTest.class));
-        linkedHashMap.put("29", new MainItem("SwipeBack", "详细解说一下", SwipeBackMainActivity.class));
-        linkedHashMap.put("30", new MainItem("DrawerTest", "详细解说一下", DrawerLayoutActTest.class));
-        linkedHashMap.put("31", new MainItem("View转换成bitmap", "详细解说一下", ViewToBitmapAct.class));
-        linkedHashMap.put("32", new MainItem("本地记录Log", "", LogTest.class));
-        linkedHashMap.put("33", new MainItem("WindowTest", "", WindowTest.class));
-        linkedHashMap.put("34", new MainItem("HandlerThread", "详细解说一下", TestHandlerThreadAct.class));
-        linkedHashMap.put("35", new MainItem("TestIntentService", "详细解说一下", TestIntentServiceAct.class));
-        linkedHashMap.put("36", new MainItem("ViewTest", "", CommonViewTest.class));
-        linkedHashMap.put("37", new MainItem("HtmlTest", "", HtmlAllTestActivity.class));
+        map = new TreeMap<>((o1, o2) -> Integer.parseInt(o2) - Integer.parseInt(o1));
+        map.put("56", new MainItem("UI线程卡顿监听", "Looper打印 消息列表 Choreographer", MonitorUiBlockActivity.class));
+        map.put("55", new MainItem("apache common包测试", "BeanUtils CollectionsUtils", ApacheCommonTestAct.class));
+        map.put("54", new MainItem("线程池", "吼吼吼", ThreadPoolAct.class));
+        map.put("53", new MainItem("视频播放", "MediaPlayer配合surfaceView", MediaPlayerTest.class));
+        map.put("52", new MainItem("下载安装测试", "下载完成之后，自动安装此app", DownTest.class));
+        map.put("51", new MainItem("CollapsingToolbarLayout", "", CollapsingToolbarLayoutTest.class));
+        map.put("50", new MainItem("约束性布局", "ConstraintLayout", ConstraintLayoutTest.class));
+        map.put("49", new MainItem("状态栏适配变色", "", StatusBarFitColorAct.class));
+        map.put("48", new MainItem("link测试", "link", AppLinkTestAct.class));
+        map.put("47", new MainItem("binding测试", "6:绑定容器 数组", Test6Act.class));
+        map.put("46", new MainItem("binding测试", "5:View随model变化 而变化 ", Test5Act.class));
+        map.put("45", new MainItem("binding测试", "4:绑定多个相同的model ", Test4Act.class));
+        map.put("44", new MainItem("binding测试", "3:静态方法调用.运算符操作数据 ", Test3Act.class));
+        map.put("43", new MainItem("binding测试", "2:事件绑定 ", Test2Act.class));
+        map.put("42", new MainItem("binding测试", "1:测试数据绑定 ", Test1Act.class));
+        map.put("41", new MainItem("临时测试", "临时测试 ", MainActivity.class));
+        map.put("40", new MainItem("java upper", "lambda测试 ", JavaUpperAct.class));
+        map.put("39", new MainItem("WebView总结", "android与js的相互调用测试 ", WebViewAct.class));
+        map.put("1", new MainItem("矢量动画", "VectorSVG可缩放矢量动画 objectAnimator", VectorSVGAct.class));
+        map.put("2", new MainItem("事件分发测试", MotionEventAct.class));
+        map.put("3", new MainItem("drawable style效果", "详细解说一下", DrawableStyleAct.class));
+        map.put("4", new MainItem("drawable效果  包括BitmapShader", "详细解说一下", DrawableAct.class));
+        map.put("5", new MainItem("选图测试", "详细解说一下", ChooseImgTestAct.class));
+        map.put("6", new MainItem("左侧滑动", "详细解说一下", SwipeBackMainActivity.class));
+        map.put("7", new MainItem("自定义layout", "详细解说一下", MyLayoutAct.class));
+        map.put("8", new MainItem("自定义View", "详细解说一下", ViewLocationAct.class));
+        map.put("38", new MainItem("显示圆形图", "", CustomRoungImgAct.class));
+        map.put("9", new MainItem("SurfaceView测试", "详细解说一下", SurfaceViewTestAct.class));
+        map.put("10", new MainItem("测试广播", "详细解说一下", BroadCastActTest.class));
+        map.put("11", new MainItem("测试ConfigChanges", "详细解说一下", ConfigChangesActTest.class));
+        map.put("12", new MainItem("测试Handler处理", "详细解说一下", HandlerTest.class));
+        map.put("13", new MainItem("测试通知 类似微博效果", "详细解说一下", TestNotificationAct.class));
+        map.put("14", new MainItem("调通dex的方法", "详细解说一下", LoaderAct.class));
+        map.put("15", new MainItem("加载超大图", "详细解说一下", LargeImageViewActivity.class));
+        map.put("16", new MainItem("自定义ViewPager显示卷图", "详细解说一下", ParallaxVpTestActivity.class));
+        map.put("17", new MainItem("glide加载图片 测试", "详细解说一下", GlideTestAct.class));
+        map.put("18", new MainItem("material小特性", "translationZ  elevation", Material1Act.class));
+        map.put("19", new MainItem("recycle", "详细解说一下", RecycleAct.class));
+        map.put("20", new MainItem("cardview", "详细解说一下", CardViewAct.class));
+        map.put("21", new MainItem("service", "详细解说一下", ServiceActivity.class));
+        map.put("22", new MainItem("ContentProvider", "详细解说一下", TestCPActivity.class));
+        map.put("23", new MainItem("Synchonized", "测试synchonized", SynchonizedTest.class));
+        map.put("24", new MainItem("反射", "详细解说一下", ReflectTest.class));
+        map.put("25", new MainItem("messenger", "msgFromClient.replyTo = mMessenger", ActivityMessenger.class));
+        map.put("26", new MainItem("AccessbilityService", "详细解说一下", AccessServiceAct.class));
+        map.put("27", new MainItem("子线程操作UI", "详细解说一下", OperateUiThreadAct.class));
+        map.put("28", new MainItem("CanvasTest", "详细解说一下", CanvasTest.class));
+        map.put("29", new MainItem("SwipeBack", "详细解说一下", SwipeBackMainActivity.class));
+        map.put("30", new MainItem("DrawerTest", "详细解说一下", DrawerLayoutActTest.class));
+        map.put("31", new MainItem("View转换成bitmap", "详细解说一下", ViewToBitmapAct.class));
+        map.put("32", new MainItem("本地记录Log", "", LogTest.class));
+        map.put("33", new MainItem("WindowTest", "", WindowTest.class));
+        map.put("34", new MainItem("HandlerThread", "详细解说一下", TestHandlerThreadAct.class));
+        map.put("35", new MainItem("TestIntentService", "详细解说一下", TestIntentServiceAct.class));
+        map.put("36", new MainItem("ViewTest", "", CommonViewTest.class));
+        map.put("37", new MainItem("HtmlTest", "", HtmlAllTestActivity.class));
     }
 
 
     private void show() {
-        final Collection<MainItem> valueItems = linkedHashMap.values();
-        final Set<String> keySet = linkedHashMap.keySet();
+        final Collection<MainItem> valueItems = map.values();
+        final Set<String> keySet = map.keySet();
 
-        final MainItem[] itemArray = new MainItem[linkedHashMap.size()];
-        final String[] keyArray = new String[linkedHashMap.size()];
+        final MainItem[] itemArray = new MainItem[map.size()];
+        final String[] keyArray = new String[map.size()];
 
         valueItems.toArray(itemArray);
         keySet.toArray(keyArray);
 
-        for (int i = 0; i < linkedHashMap.size(); i++) {
+        for (int i = 0; i < map.size(); i++) {
             itemArray[i].setTitle(keyArray[i] + " " + itemArray[i].getTitle());
         }
         final MainRecycleAdapter mAdapter = new MainRecycleAdapter(itemArray);
@@ -260,7 +238,6 @@ public class MainAct extends BaseActivity {
             public void onItemClck(int i) {
                 getWindow().setExitTransition(new Explode());
                 Intent intent = new Intent(mAct, itemArray[i].getmAct());
-
                 startActivity(intent);
 //                startActivity(intent,
 //                        ActivityOptions

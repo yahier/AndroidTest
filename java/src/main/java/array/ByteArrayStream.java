0e630f11bd7a487dda9b9cd9a008d92939549b3b
xuziwang <xuziwang@xhg.com>
2019-03-06 16:38:46 +0800

    修改byte[]和String互转时 不同字符编码 的测试

--- java/src/main/java/array/ByteArrayStream.java
+++ java/src/main/java/array/ByteArrayStream.java
@@ -5,29 +5,53 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 /**
- * 如何将byte[]转换称String,内容不变呢。
+ * 如何将byte[]转换称String,内容不变呢。使用 ISO-8859-1 编码
+ * 由于 UTF-8 是多字节编码，需要用多个字节来表示一个字符的编码，所以也就出现了在转换之后 byte[] 数组长度、内容不一致的情况。
+ * <p>
+ * 而 ISO-8859-1 编码是单字节编码，所以使用该编码就不会出现上面的问题
  */
 public class ByteArrayStream {
+    static byte[] data = {66, 77, -50, 33, 40, 37, 34, 41, 38, 31, 38, 35, 28, 35,
+            34, 60, 69, 68, 95, 103, 104, 111, 119, 120, -128, -121, -118,
+            -124, -117, -114, -119, -112, -107, -121, -114, -109, -123,
+            -117};
+
+
     public static void main(String[] args) {
+        String charset = Charset.defaultCharset().name();//UTF-8
+        System.out.println("charset:" + charset);
         test();
     }
 
+    /**
+     * 将byte[]转成string，再专程byte[] ，怎么变化了呢
+     */
     static void test() {
-        byte[] data = {66, 77, -50, 33, 40, 37, 34, 41, 38, 31, 38, 35, 28, 35,
-                34, 60, 69, 68, 95, 103, 104, 111, 119, 120, -128, -121, -118,
-                -124, -117, -114, -119, -112, -107, -121, -114, -109, -123,
-                -117};
+        String t = null;
+        {
+            t = new String(data);
+            byte[] c = t.getBytes();
+            System.out.println("c:" + Arrays.toString(c));//内容和原data数组不同
+        }
 
-        String t = new String(data);
-        System.out.println("t:" + t);
-        byte[] c = t.getBytes();
-        System.out.println("c:" + Arrays.toString(c));
+        {
+            try {
+                t = new String(data, "ISO-8859-1");
+                byte[] c = t.getBytes("ISO-8859-1");
+                System.out.println("c:" + Arrays.toString(c));//内容和原data数据相同
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
-    static void s(byte[] data) {
+    static void test2() {
         ByteArrayInputStream out = new ByteArrayInputStream(data);
         String con = out.toString();
         System.out.println(con);
